91eb9c7331334d51dc008ef40b617b9413fc727a
hiber63 <39085603@qq.com>
2019-09-27 23:39:56 +0800

    init starter

---
+++ src/main/java/cn/hiber/pay/HiberPayApplication.java
@@ -0,0 +1,13 @@
+package cn.hiber.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiberPayApplication.class, args);
+    }
+
+}
